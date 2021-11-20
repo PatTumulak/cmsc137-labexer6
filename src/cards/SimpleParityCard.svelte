025fe32f17a7acef116ad4e14bbdc49b7fbf9063
Patricia Tumulak <pat@bposeats.com>
2021-11-20 11:32:59 +0800

    Added functionality for Simple Parity

--- src/cards/SimpleParityCard.svelte
+++ src/cards/SimpleParityCard.svelte
@@ -1,12 +1,42 @@
 <script>
     let simpleParityA = '';
 	let simpleParityB = '';
+	let outputA = '';
+	let outputB = '';
 
 	function isValidBinary(input) {
 		for (let i of input) {
-			if (i !== '0'&& i != '1') return false;
+			if (i !== '0'&& i != '1') {
+				return false;
+			}
 		}
 		return true;
+	}
+
+	function generateParity(input) {
+		let sum = 0;
+
+		for (let i of input) {
+			if (i === 1) {
+				sum++;
+			}
+		}
+		let parity = sum%2 === 0 ? '0' : '1';
+		return parity;
+	}
+
+	function getParityBit(input) {
+		// console.log('parity bit', input[input.length - 1]);
+		return input[input.length - 1];
+	}
+
+	function checkSyndrome(input) {
+		let receivedParity = getParityBit(input);
+		let neededParity = generateParity(input.substring(0, input.length-1));
+
+		// console.log('lol', input.substring(0, input.length-1));
+
+		return receivedParity === neededParity ? true : false;
 	}
 
 	function handleClick() {
@@ -17,8 +47,25 @@
 				&& simpleParityB.length === 9 ? true : false;
 
 			if (inputValidityA && inputValidityB) {
-				console.log(simpleParityA);
-				console.log(simpleParityB);
+				// input A stuff
+				// generates the parity bit
+				let parityA = generateParity(simpleParityA);
+				let dataWord = simpleParityA + parityA;	
+				
+				// input B stuff
+				// checks the parity bit and generates an actual parity bit to
+				// compare
+				let syndromeCheck = checkSyndrome(simpleParityB);
+
+				outputA = dataWord;
+				outputB = syndromeCheck === true ? simpleParityB.substring(
+					0, simpleParityB.length-2) : 'Discarded';
+
+				// console.log('parity', parityA);
+				// console.log(simpleParityA);
+				// console.log(simpleParityB);
+				// console.log('output', outputA);
+				// console.log('output', outputB);
 			} else {
 				console.log('hatdog');
 			}
@@ -38,8 +85,8 @@
 
     <button on:click={handleClick}>Check</button>
 
-    <p>Output</p>
-    <p></p>
+    <p>Output A: { outputA }</p>
+	<p>Output B: { outputB }</p>
 </div>
 
 <style>
